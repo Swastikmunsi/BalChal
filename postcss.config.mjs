c3a46e17559a91d2cc08c22caed7cfc7926fa530
Swastik Munsi <swastik00111@gmail.com>
2025-08-24 23:59:07 +0530

    Update postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
-=======
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
->>>>>>> 9211fab7b5abdca8bac92f3d031e8e0161ed406b
